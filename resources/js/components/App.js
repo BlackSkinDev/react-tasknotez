9325b08ca8b562e84f900fb6a000c367b03f48f7
Afeez Azeez <chillingloccini1@gmail.com>
2022-01-11 21:48:18 +0100

    rendering tasks in TaskList Component, And implementing rearranging of task on frontend

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -7,6 +7,7 @@ import TaskList from "./pages/tasks/TaskList"
 import TaskCreate from "./pages/tasks/TaskCreate"
 import TaskEdit from "./pages/tasks/TaskEdit"
 
+
 import NotFound from "./pages/NotFound"
 import {Button,Container} from 'react-bootstrap'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
@@ -14,30 +15,6 @@ import {PUBLIC_URL} from "../constants"
 import Swal from 'sweetalert2'
 
 class App extends Component {
-
-    componentDidMount(){
-       // this.x()
-    }
-
-    x = ()=>{
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-              )
-            }
-          })
-    }
 
   render() {
     return (
@@ -49,7 +26,7 @@ class App extends Component {
 
             <Container className="p-4">
               <Switch>
-                <Route path={`${PUBLIC_URL}`} exact={true} component={TaskList} />
+                <Route path="/" exact={true} component={TaskList} />
                 <Route path={`${PUBLIC_URL}`} exact={true} component={TaskList} />
                 <Route path={`${PUBLIC_URL}/create`} exact={true} component={TaskCreate} />
                 <Route path={`${PUBLIC_URL}/edit-task/:id`} component={TaskEdit}/>
--- resources/js/components/pages/tasks/TaskList.js
+++ resources/js/components/pages/tasks/TaskList.js
@@ -3,25 +3,150 @@ import React, { Component } from 'react'
 import Axios from 'axios'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {Spinner,Button} from 'react-bootstrap'
+import {Spinner,Button,Card,Badge} from 'react-bootstrap'
+
 import { Link } from "react-router-dom";
 import {storeNewTask} from "../../../services/TaskService";
 import {getTaskLists} from "../../../services/TaskService";
 
-import Task from "./Task"
+import {sortableContainer, sortableElement} from 'react-sortable-hoc';
+import { arrayMove } from 'react-sortable-hoc';
+
+import Swal from 'sweetalert2'
+
+import {FaEye } from 'react-icons/fa';
+
+
+
+
+
 
 import  '../../asset/style.css';
+
+const BASE_URL = process.env.MIX_REACT_APP_BASE_URL;
+
+const SortableContainer = sortableContainer(({children}) => {
+    return <div>{children}</div>;
+  });
+
+function setAsIncomplete(taskId){
+    Swal.fire({
+        title: 'Set task as incomplete?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            Axios.get(`${BASE_URL}/tasks/${taskId}/unset`)
+            .then(response=>{
+                Swal.fire(
+                    'Done!',
+                    response.data.message,
+                    'success'
+                  )
+                  .then((result) => {
+                    location.reload();
+                  });
+
+            })
+            .catch(err =>{
+                Swal.fire(
+                    'Error',
+                    'Error encountered while processing!',
+                    'error'
+                  )
+            })
+
+        }
+     })
+}
+
+function setAsCompleted(taskId){
+    Swal.fire({
+        title: 'Set task as complete?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            Axios.get(`${BASE_URL}/tasks/${taskId}/set`)
+            .then(response=>{
+                Swal.fire(
+                    'Done!',
+                    response.data.message,
+                    'success'
+                  )
+                  .then((result) => {
+                    location.reload();
+                  });
+
+            })
+            .catch(err =>{
+                Swal.fire(
+                    'Error',
+                    'Error encountered while processing!',
+                    'error'
+                  )
+
+            })
+        }
+     })
+}
+
+  const SortableItem = sortableElement(({task}) =>   <Card className="mt-4 task-card" key={task.id}>
+            <Card.Body>
+                <Card.Title>
+                </Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                <p>
+                    <small>Created on: {task.created_at}</small>
+                    { task.completed_at==null?
+                        <Badge bg="warning" className="status-badge float-right" >Ongoing</Badge>:
+                        <Badge bg="success" className="status-badge float-right" >Completed</Badge>
+                    }
+                </p>
+
+                {task.completed_at &&
+                    (<p><small>Completed on: {task.completed_at}</small></p>)
+
+                }
+                </Card.Subtitle>
+                <Card.Text>
+                {task.label}
+                </Card.Text>
+                <Link to={`/tasks/edit-task/${task.id}`} >
+                    <Button variant="outline-primary" className="button-text">Edit</Button>
+                </Link>
+                {task.completed_at==null?
+                <Button variant="outline-success" className="ml-4 button-text" id={task.id} onClick={e =>setAsCompleted(e.target.id)}>Complete</Button>:
+                <Button variant="outline-danger" className="ml-4 button-text" id={task.id} onClick={e => setAsIncomplete(e.target.id)}>Uncomplete</Button>
+                }
+                </Card.Body>
+            </Card>);
+
+
+
+
 
 class TaskList extends Component {
     state ={
         taskList: [],
         counter:1,
         BASE_URL:process.env.MIX_REACT_APP_BASE_URL,
-        isLoading:false
+        isLoading:false,
+        label:'',
+        errors:[],
+        taskLabel: '',
+
     }
 
     componentDidMount() {
         this.getTasksLists()
+
     }
 
     getTasksLists = async () => {
@@ -32,6 +157,7 @@ class TaskList extends Component {
             taskList: response.data,
             isLoading: false,
         });
+
         } else {
         this.setState({
             isLoading: false,
@@ -40,6 +166,19 @@ class TaskList extends Component {
         alert("An Error was encountered, try again later")
        }
     };
+
+
+    onSortEnd = ({oldIndex, newIndex}) => {
+       // console.log("Old index:" + oldIndex + " new index:" + newIndex)
+        console.log(this.state.taskList)
+        this.setState(({taskList}) => ({
+          taskList: arrayMove(this.state.taskList, oldIndex, newIndex),
+        }));
+       // console.log(this.state.taskList)
+       console.log(this.state.taskList)
+      };
+
+
 
     render() {
         return (
@@ -58,18 +197,23 @@ class TaskList extends Component {
                         </Spinner>
                     </div>
                 )}
+
+
                 { this.state.taskList.length > 0 ?
 
-                <Row className="g-3" className='mt-4 justify-content-center'>
+                    <Row className="g-3" className='mt-4 justify-content-center'>
                     <Col xs={12} md={7}>
-                    { Object.entries(this.state.taskList).map(([key, task]) => {
-                        return (
-                            <Task task={task}/>)
-                        })}
-                    </Col>
-                </Row>
+                    <SortableContainer onSortEnd={this.onSortEnd}>
+                    {this.state.taskList.map((task, index) => (
+                          <SortableItem key={`item-${task.id}`} index={index} task={task} />
 
-                :<h4 className="mt-5">Oops! You have no task</h4>
+                     ))}
+                       </SortableContainer>
+                    </Col>
+
+                    </Row>
+
+                    :<h4 className="mt-5">Oops! You have no task</h4>
                 }
 
 
