71ed3de68defb0baf59cb02599d34d5477307128
Afeez Azeez <chillingloccini1@gmail.com>
2022-01-13 05:58:06 +0100

    upgraded to laravel 8

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Response;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -37,7 +38,7 @@ class Handler extends ExceptionHandler
      *
      * @throws \Exception
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
@@ -51,7 +52,7 @@ class Handler extends ExceptionHandler
      *
      * @throws \Exception
      */
-    public function render($request, Exception $e)
+    public function render($request,Throwable $e)
     {
         if($e instanceof  ModelNotFoundException || $e instanceof NotFoundHttpException){
             return response()->json(['message'=>'Resource not found'],Response::HTTP_NOT_FOUND);
